955d82e53b2d3ec9e80bbe3c9f3fa8a3b028ccc6
danielgomezcantero <danielgomezcantero@gmail.com>
2020-07-22 12:58:46 -0400

    Exponer Animationcontroller mediante Provider

--- animate_do_app/animate_do_app/lib/src/pages/navegacion_page.dart
+++ animate_do_app/animate_do_app/lib/src/pages/navegacion_page.dart
@@ -2,7 +2,7 @@ import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:provider/provider.dart';
-import 'package:transparent_image/transparent_image.dart';
+//import 'package:transparent_image/transparent_image.dart';
 
 class NavegationPage extends StatelessWidget {
   @override
@@ -17,6 +17,7 @@ class NavegationPage extends StatelessWidget {
             width: 100,
             height: 100,
             child: ElasticInDown(
+              delay: Duration(milliseconds: 1000),
               child: Image.asset(
                 "assets/images/logoPalermo.png",
                 fit: BoxFit.contain,
@@ -50,19 +51,26 @@ class BottomNavigation extends StatelessWidget {
                 top: 0.0,
                 right: 0.0,
                 // child: Icon(Icons.brightness_1, size: 8,color: Colors.red,),
+
                 child: BounceInDown(
                   from: 10,
                   animate: (numeroNotification > 0) ? true : false,
-                  child: Container(
-                    child: Text(
-                      '$numeroNotification',
-                      style: TextStyle(color: Colors.white, fontSize: 8),
+                  child: Bounce(
+                    from: 10,
+                    controller: (controller) =>
+                        Provider.of<_NotificationModel>(context)
+                            .bounceController = controller,
+                    child: Container(
+                      child: Text(
+                        '$numeroNotification',
+                        style: TextStyle(color: Colors.white, fontSize: 8),
+                      ),
+                      alignment: Alignment.center,
+                      width: 13,
+                      height: 13,
+                      decoration: BoxDecoration(
+                          color: Colors.redAccent, shape: BoxShape.circle),
                     ),
-                    alignment: Alignment.center,
-                    width: 13,
-                    height: 13,
-                    decoration: BoxDecoration(
-                        color: Colors.redAccent, shape: BoxShape.circle),
                   ),
                 ),
               )
@@ -76,30 +84,48 @@ class BottomNavigation extends StatelessWidget {
   }
 }
 
+//Botones flotante
 class BotonFlotante extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return FloatingActionButton(
-        backgroundColor: Colors.pink,
-        child: FaIcon(FontAwesomeIcons.play),
-        onPressed: () {
-          int numero =
-              Provider.of<_NotificationModel>(context, listen: false).numero;
-          numero++;
+    return BounceInDown(
+      from: 10,
+      child: FloatingActionButton(
+          backgroundColor: Colors.pink,
+          child: FaIcon(FontAwesomeIcons.play),
+          onPressed: () {
+            final notiModel =
+                Provider.of<_NotificationModel>(context, listen: false);
 
-          Provider.of<_NotificationModel>(context, listen: false).numero =
-              numero;
-        });
+            int numero = notiModel.numero;
+            numero++;
+
+            notiModel.numero = numero;
+
+            if (numero >= 2) {
+              final controller = notiModel.bounceController;
+              controller.forward(from: 0.0);
+            }
+          }),
+    );
   }
 }
 
 class _NotificationModel extends ChangeNotifier {
   int _numero = 0;
+  AnimationController _bounceController;
 
-  get numero => this._numero;
+  int get numero => this._numero;
 
   set numero(int valor) {
     this._numero = valor;
+    notifyListeners();
+  }
+
+  AnimationController get bounceController => this._bounceController;
+
+  set bounceController(AnimationController controller) {
+    this._bounceController = controller;
     notifyListeners();
   }
 }
